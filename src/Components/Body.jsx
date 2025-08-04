318357670c0d473997625b33d743180905c12c05
Aniket Rana <aniketrana7681@gmail.com>
2025-08-04 17:31:56 +0530

    upd

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,50 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import Footer from "./Footer";
+// import Navbar from "./Navbar";
+// import { Outlet, useNavigate } from "react-router-dom";
+// import { BASE_URL } from "../Utils/constants";
+// import { addUser } from "../Utils/userSlice";
+// import { useEffect } from "react";
+// import axios from "axios";
+
+// const Body = () => {
+//   const userData = useSelector((store) => store.user);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const fetchUser = async () => {
+//     if (userData) return;
+//     try {
+//       const res = await axios.get(BASE_URL + "/profile/view", {
+//         withCredentials: true,
+//       });
+//       dispatch(addUser(res.data));
+//     } catch (err) {
+//       if (err.response.status === 401) {
+//         navigate("/login");
+//       } else {
+//         console.log("Error:" + err.message);
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchUser();
+//   }, []);
+//   return (
+//     <div>
+//       <Navbar />
+//       <Outlet />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Body;
+
 import { useDispatch, useSelector } from "react-redux";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { BASE_URL } from "../Utils/constants";
 import { addUser } from "../Utils/userSlice";
 import { useEffect } from "react";
@@ -11,24 +54,31 @@ const Body = () => {
   const userData = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+
   const fetchUser = async () => {
-    if (userData) return;
+    // Only fetch if userData is empty
+    if (Object.keys(userData).length !== 0) return;
+
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
       dispatch(addUser(res.data));
     } catch (err) {
-      if (err.status === 401) {
+      if (err.response && err.response.status === 401) {
         navigate("/login");
+      } else {
+        console.error("Error:", err.message);
       }
-      console.log("Error:" + err.message);
     }
   };
 
+  // Call on every path change to support navigation
   useEffect(() => {
     fetchUser();
-  }, []);
+  }, [location.pathname]);
+
   return (
     <div>
       <Navbar />
